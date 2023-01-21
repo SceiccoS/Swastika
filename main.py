cc4f1e1458a74bf46009dab0699971c8683e5d27
SceiccoS <85369956+SceiccoS@users.noreply.github.com>
2023-01-21 15:58:41 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+def Svastica(row,col):
+	
+	for i in range(row):
+		for j in range(col):
+			
+			
+			if(i < row // 2):
+				
+				
+				if (j < col // 2):
+					
+					
+					if (j == 0):
+						print("*", end = "")
+						
+					
+					else:
+						print(" ", end = " ")
+				
+				
+				elif (j == col // 2):
+					print(" *", end = "")
+				else:
+					
+					
+					if (i == 0):
+						print(" *", end = "")
+						
+			elif (i == row // 2):
+				print("* ", end = "")
+			else:
+				
+				
+				if (j == col // 2 or j == col - 1):
+					print("* ", end = "")
+					
+				
+				elif (i == row - 1):
+					
+				
+					if (j <= col // 2 or j == col - 1):
+						print("* ", end = "")
+					else:
+						print(" ", end = " ")
+				else:
+					print(" ", end = " ")
+		print()
+		
+
+row = 11; col = 11
+
+Svastica(row, col)
